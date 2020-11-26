0320e20ac7582d3328ec5418dd77aba97498b47d
jack <jackzhoumine@gmail.com>
2020-11-27 03:03:38 +0800

    404路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@
  * @Description: 路由模块
  * @Date: 2020-11-27 00:54:02 +0800
  * @Author: JackChouMine
- * @LastEditTime: 2020-11-27 02:35:45 +0800
+ * @LastEditTime: 2020-11-27 03:02:21 +0800
  * @LastEditors: JackChouMine
  */
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component'
+import { NotFoundComponent } from './not-found/not-found.component'
 import { ProductComponent } from './product/product.component'
 
 const routes: Routes = [
@@ -18,6 +19,10 @@ const routes: Routes = [
   {
     path: 'product', // NOTE 不能以 / 开头
     component: ProductComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent, // NOTE 404 路由，必须放在最后
   },
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeComponent } from './home/home.component';
-import { ProductComponent } from './product/product.component'
+import { ProductComponent } from './product/product.component';
+import { NotFoundComponent } from './not-found/not-found.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProductComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
