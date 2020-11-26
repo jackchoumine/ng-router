5ddb35bc02659d0ed60a3852d77eb202e232521c
jack <jackzhoumine@gmail.com>
2020-11-27 02:59:12 +0800

    路由导航

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
+/*
+ * @Description: 根组件
+ * @Date: 2020-11-27 00:54:02 +0800
+ * @Author: JackChouMine
+ * @LastEditTime: 2020-11-27 02:59:08 +0800
+ * @LastEditors: JackChouMine
+ */
 import { Component } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,13 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-router'
+  // NOTE 注入路由
+  constructor(private router: Router){
+
+  }
+  toProduct(): void{
+    console.log('去到商品详情')
+    this.router.navigate(['/product'])
+  }
 }
+
