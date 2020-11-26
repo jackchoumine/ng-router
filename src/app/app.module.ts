8d8e109a683f5092895528f9dca4b52802a0f689
jack <jackzhoumine@gmail.com>
2020-11-27 03:45:47 +0800

    路径传递参数

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,8 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HomeComponent } from './home/home.component';
-import { ProductComponent } from './product/product.component';
+import { HomeComponent } from './home/home.component'
+import { ProductComponent } from './product/product.component'
 import { NotFoundComponent } from './not-found/not-found.component'
 
 @NgModule({
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -2,7 +2,7 @@
  * @Description: 商品组件
  * @Date: 2020-11-27 02:32:40 +0800
  * @Author: JackChouMine
- * @LastEditTime: 2020-11-27 03:29:39 +0800
+ * @LastEditTime: 2020-11-27 03:45:30 +0800
  * @LastEditors: JackChouMine
  */
 import { Component, OnInit } from '@angular/core'
@@ -14,12 +14,15 @@ import { ActivatedRoute } from '@angular/router'
   styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
-  private productId: number // TODO  声明在构造函数中，报错
+  productId: number // TODO  声明在构造函数中，报错
+  productName: string
   constructor(private activeRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     console.log(this.activeRoute)
     // NOTE 使用路由快照获取参数
+    this.productName = this.activeRoute.snapshot.params.productName
     this.productId = this.activeRoute.snapshot.queryParams.id
+    //
   }
 }
